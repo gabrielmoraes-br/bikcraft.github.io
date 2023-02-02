1c84c400f404b528c7dd89f7b38df4127ea46eb8
Gabriel Moraes <gabrielmoraes.dev@gmail.com>
2023-02-02 19:48:21 -0300

    Optimized for production

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 //Enable nav links
-const links = document.querySelectorAll('.header-nav a');
+const links = document.querySelectorAll(".header-nav a");
 
 function linkEnabled(link) {
   if (location.href.includes(link.href)) {
-    link.classList.add('enabled');
+    link.classList.add("enabled");
   }
 }
 
 links.forEach(linkEnabled);
 
-//Enable budget items
+//Enable quote items
 
 const parameters = new URLSearchParams(location.search);
 
@@ -24,20 +24,20 @@ parameters.forEach(itemEnabled);
 
 //Collapse faq items
 
-const questions = document.querySelectorAll('.faq button');
+const questions = document.querySelectorAll(".faq button");
 
 function enableAnswer(question) {
-  question.addEventListener('click', showAnswer);
+  question.addEventListener("click", showAnswer);
 }
 
 function showAnswer(event) {
   const question = event.currentTarget;
-  const controls = question.getAttribute('aria-controls');
+  const controls = question.getAttribute("aria-controls");
   const answer = document.getElementById(controls);
 
-  answer.classList.toggle('enabled');
-  const enabled = answer.classList.contains('enabled');
-  question.setAttribute('aria-expanded', enabled);
+  answer.classList.toggle("enabled");
+  const enabled = answer.classList.contains("enabled");
+  question.setAttribute("aria-expanded", enabled);
 
   console.log(answer);
 }
@@ -46,16 +46,16 @@ questions.forEach(enableAnswer);
 
 // Bike img gallery
 
-const gallery = document.querySelectorAll('.bike-img img');
-const galleryContainer = document.querySelector('.bike-img');
+const gallery = document.querySelectorAll(".bike-img img");
+const galleryContainer = document.querySelector(".bike-img");
 
 function selectImage(image) {
-  image.addEventListener('click', setImage);
+  image.addEventListener("click", setImage);
 }
 
 function setImage(event) {
   const image = event.currentTarget;
-  const media = matchMedia('(min-width: 937px)').matches;
+  const media = matchMedia("(min-width: 937px)").matches;
   if (media) {
     galleryContainer.prepend(image);
   }
